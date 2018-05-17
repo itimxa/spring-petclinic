3b24350611b561fa8ef5370d512603d0086bac9f
Vadym Tymoshenko <itimxa@gmail.com>
2018-05-17 20:56:49 +0300

    Fix check script Rewrite it to python3

--- scripts/check.py
+++ scripts/check.py
@@ -3,22 +3,17 @@ import json
 import time
 import boto3
 
-# file = open('hosts', 'r')
-# string = file.readlines()
-#
-# url = string[3] + '/manage/health'
-# file.close()
 
 res = boto3.resource('ec2')
 instances = res.instances.filter(Filters = [{'Name': 'instance-state-name', 'Values': ['running']},
-                {'Name': 'tag:Name', 'Values': ['application']}]))
+                {'Name': 'tag:Name', 'Values': ['application']}])
 
 for i in instances:
-    url = i.public_ip_address + '/manage/health'
-    for i in range(6):
+    url =  'http://' + i.public_ip_address + ':8080/manage/health'
+    for i in range(1):
         try:
-            request = urllib.urlopen(url)
-            data = json.loads(request.read())
+            request = urllib.request.urlopen(url)
+            data = json.loads(request.read().decode('utf-8'))
             break
 
         except IOError:
@@ -26,4 +21,4 @@ for i in instances:
             continue
     
     if data['status'] == 'UP':
-        print "Service is up"
+        print ("Service is up")
